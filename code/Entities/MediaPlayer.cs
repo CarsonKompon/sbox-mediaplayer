0b48cb685bf4ae3e5cea0e78c84173ef0d4464b6
Carson Kompon <carsokompo@gmail.com>
2023-06-23 21:26:58 -0400

    Added Queue and Queue UI

--- code/Entities/MediaPlayer.cs
+++ code/Entities/MediaPlayer.cs
@@ -14,17 +14,23 @@ namespace CarsonK;
 /// A placeable TV that you can queue media on
 /// </summary>
 [EditorModel("models/mediaplayer_dev.vmdl")]
-[Library]
+[Library("carson_mediaplayer"), HammerEntity]
 public partial class MediaPlayer : ModelEntity, IUse
 {
 
     public virtual bool IsUsable( Entity user ) => true;
-    [Net] public List<string> Queue {get; set;} = new();
-    [Net] public string CurrentlyPlaying { get; set; } = "";
+    [Net] public List<MediaVideo> Queue {get; set;} = new();
+    [Net] public MediaVideo CurrentlyPlaying { get; set; }
     [Net] public float CurrentLength { get; set; } = 5;
     [Net] public RealTimeSince VideoStart { get; set; } = 0;
     [Net] public bool IsPlaying { get; set; } = false;
     private bool LoadingVideo { get; set; } = false;
+
+    /// <summary>
+    /// Physics motion type.
+    /// </summary>
+    [Property( Title = "Physics Type" )]
+    private PhysicsMotionType MotionType {get; set;} = PhysicsMotionType.Dynamic;
 
     public VideoPlayer Video { get; set; }
     public Material ScreenMaterial { get; set; }
@@ -38,7 +44,7 @@ public partial class MediaPlayer : ModelEntity, IUse
     {
         base.Spawn();
         SetModel("models/mediaplayer_dev.vmdl");
-        SetupPhysicsFromModel(PhysicsMotionType.Dynamic);
+        SetupPhysicsFromModel(MotionType);
     }
 
     protected override void OnDestroy()
@@ -52,14 +58,12 @@ public partial class MediaPlayer : ModelEntity, IUse
     {
         if(IsPlaying && Video.PlaybackTime > 0 && Video.PlaybackTime > Video.Duration)
         {
-            Video.Dispose();
-            Video = null;
-            IsPlaying = false;
+            SkipCurrentAll();
         }
-        if(!LoadingVideo && !IsPlaying && Queue.Count() > 0 && VideoStart >= CurrentLength)
+        if(!LoadingVideo && !IsPlaying && Queue.Count() > 0 && Video == null)
         {
             CurrentlyPlaying = Queue[0];
-            PlayVideoForAll(CurrentlyPlaying);
+            PlayVideoForAll(CurrentlyPlaying.Url);
             Queue.RemoveAt(0);
             VideoStart = 0f;
             LoadingVideo = true;
@@ -85,7 +89,7 @@ public partial class MediaPlayer : ModelEntity, IUse
     }
 
     [ConCmd.Server]
-    public static void QueueMedia(int networkIdent, string url)
+    public static async void QueueMedia(int networkIdent, string url)
     {
         var entity = Entity.FindByIndex(networkIdent);
         if(entity is not MediaPlayer mediaPlayer)
@@ -94,14 +98,61 @@ public partial class MediaPlayer : ModelEntity, IUse
             return;
         }
 
+        MediaVideo video = await MediaVideo.CreateFromUrl(url);
+
         // Queue the media
-        mediaPlayer.Queue.Add(url);
+        mediaPlayer.Queue.Add(video);
+    }
+
+    [ConCmd.Server]
+    public static async void RemoveMedia(int networkIdent, string url)
+    {
+        var entity = Entity.FindByIndex(networkIdent);
+        if(entity is not MediaPlayer mediaPlayer)
+        {
+            Log.Error("📺: Tried to remove media from a TV that doesn't exist!");
+            return;
+        }
+
+        var where = mediaPlayer.Queue.Where(x => x.Url == url);
+
+        if(mediaPlayer.CurrentlyPlaying.Url == url)
+        {
+            mediaPlayer.SkipCurrentAll();
+        }
+        else if(where.Count() > 0)
+        {
+            mediaPlayer.Queue.Remove(where.First());
+        }
+    }
+
+    public void SkipCurrentAll()
+    {
+        
+        SkipCurrent();
+        SkipCurrentRpc();
+    }
+
+    public void SkipCurrent()
+    {
+        
+        Video.Dispose();
+        Video = null;
+        IsPlaying = false;
+        LoadingVideo = false;
+        CurrentlyPlaying = null;
+    }
+
+    [ClientRpc]
+    public void SkipCurrentRpc()
+    {
+        SkipCurrent();
     }
 
     public void PlayVideoForAll(string url)
     {
-        PlayVideo(CurrentlyPlaying);
-        PlayVideoRpc(CurrentlyPlaying);
+        PlayVideo(url);
+        PlayVideoRpc(url);
         IsPlaying = true;
     }
 
